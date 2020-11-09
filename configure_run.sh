7f35a08a52198afa070adfabe35922d5ef167a0c
MultiFox200 <vprotasenia@devforth.io>
2020-11-09 13:31:36 +0200

    Run commands fixes

--- configure_run.sh
+++ configure_run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+if [ ! -f "./nexus.json" ]; then
+    echo "nexus.json file is missing"
+    exit 1
+fi
+
 base_command="docker-compose -f docker-compose.yaml -f nexus-configure-compose.yaml"
 
 if [ "$#" -eq 0 ]; then
-    `$base_command up -d`
+    $base_command up -d
 else
-    `$base_command $@`
+    $base_command $@
 fi
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-base_command="docker-compose -f docker-compose.yaml"
+base_command="docker-compose"
 
 if [ "$#" -eq 0 ]; then
-    `$base_command up -d`
+    $base_command up -d
 else
-    `$base_command $@`
+    $base_command $@
 fi
