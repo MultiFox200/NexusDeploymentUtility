327f9e81fade882a8356b1dfa33e640691512650
MultiFox200 <vprotasenia@devforth.io>
2020-11-09 13:48:27 +0200

    If Nexus container fails python container should also exit

--- configure_run.sh
+++ configure_run.sh
@@ -8,7 +8,7 @@ fi
 base_command="docker-compose -f docker-compose.yaml -f nexus-configure-compose.yaml"
 
 if [ "$#" -eq 0 ]; then
-    $base_command up -d
+    $base_command up -d --abort-on-container-exit
 else
     $base_command $@
 fi
